a10f4562d5d73028af3040e57e0cd85791a456f4
Md. Masud Mazumder <masud.cse@engineer.com>
2022-11-15 02:03:16 +0600

    Initial commit

--- client/pages/trainee-list/index.js
+++ client/pages/trainee-list/index.js
@@ -1,7 +1,96 @@
-import React from 'react'
+// import React from "react";
+import Box from "@mui/material/Box";
+import { DataGrid } from "@mui/x-data-grid";
+import { useState } from "react";
+
+const columns = [
+  { field: "id", headerName: "ক্র.নং", width: 10 },
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    width: 110,
+    editable: true,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  },
+];
+
+const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+    { id: 11, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 12, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 13, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 14, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 15, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 16, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 17, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 18, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 19, lastName: "Roxie", firstName: "Harvey", age: 65 },
+    { id: 21, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 22, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 23, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 24, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 25, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 26, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 27, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 28, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 29, lastName: "Roxie", firstName: "Harvey", age: 65 },
+    { id: 31, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 32, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 33, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 34, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 35, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 36, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 37, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 38, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 39, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
 
 export default function Index() {
-  return (
-    <div className='h-screen bg-red-600 w-screen'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt harum omnis repellat, quia exercitationem rem iusto sit obcaecati vitae libero? Voluptatem accusamus excepturi, aut quam dolorum quasi omnis est esse doloremque quo iste sunt, amet tempora dolor vel numquam ratione cum praesentium quidem asperiores, repellat aspernatur quisquam. Illo aut ratione nihil dolorum. Reprehenderit sequi veritatis, numquam saepe aut ratione cum eveniet ipsa optio, nihil nam deleniti in! Itaque laborum ea soluta dignissimos numquam eaque quas obcaecati. Minus, et voluptatibus corporis soluta, cumque doloribus nisi dicta praesentium pariatur at recusandae alias natus cupiditate nostrum quo? Nisi dolor nostrum quisquam. Ratione, ex.</div>
-  )
+    const [pageSize, setPageSize] = useState(100);
+
+    return (
+        <Box sx={{ height: 'auto', width: "100%" }}>
+        <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={pageSize}
+            onPageSizeChange={(newPage) => setPageSize(newPage)}
+            autoHeight
+            rowsPerPageOptions={[100, 50, 20, 10, 5]}
+            checkboxSelection
+            cell--textRight
+            // disableSelectionOnClick
+            experimentalFeatures={{ newEditingApi: true }}
+        />
+        </Box>
+    );
 }
